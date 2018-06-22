606dbe7c253d10cb9fefb4098eed1a7d15e14c8b
Justine Bricknell <justinegayton@gmail.com>
2018-06-22 18:05:58 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,8 @@
 #turn your string (sentence) into an array.... .SPLIT
 #then you can run .each or .collect on that array
 def reverse_each_word(sentence)
-  sentence.split(",")
+  new_arrary = sentence.split(" ").each do |word|
+    word.reverse
+  end
+  new_array.join(" ")
 end
