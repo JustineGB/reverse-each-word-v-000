3812a8e9beaa223162697aa0b6a24f4febeb1030
Justine Bricknell <justinegayton@gmail.com>
2018-06-22 17:59:17 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,6 @@
+#reverse EACH WORD but NOT the order of the words!
+#turn your string (sentence) into an array.... .SPLIT
+#then you can run .each or .collect on that array
+def reverse_each_word(sentence)
+  sentence.split(",")
+end
